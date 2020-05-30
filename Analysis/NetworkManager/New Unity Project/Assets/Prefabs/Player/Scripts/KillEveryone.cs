a328588b27fce38e8550d8541a1179aa7d19bb56
quantumech3 <quantumech3@gmail.com>
2020-05-30 00:47:42 -0400

    Figured out how to use SyncVars

---
+++ Analysis/NetworkManager/New Unity Project/Assets/Prefabs/Player/Scripts/KillEveryone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class KillEveryone : NetworkBehaviour
+{
+    [SyncVar(hook="onValueChange")]
+    public bool everyoneIsDead = false;
+
+    public GameObject networkManagerObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isServer)
+        {
+            everyoneIsDead = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(everyoneIsDead && isClient)
+        {
+            networkManagerObject.GetComponent<NetworkManager>().StopClient();
+        }
+    }
+
+    void onValueChange(bool everyoneIsDead)
+    {
+        Debug.Log("The value was changed");
+    }
+}
---
+++ Analysis/NetworkManager/New Unity Project/Assets/Prefabs/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position.Set(transform.position.x, transform.position.y, transform.position.z);
+        //Debug.Log(transform.position);
+    }
+}
