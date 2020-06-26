222293e17df1a6b3dcfc4642a80f3f309f11bc1e
quantumech3 <quantumech3@gmail.com>
2020-06-26 00:10:48 -0400

    Implemented all fundemental AR Objects and the Origin

---
+++ Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/OriginBehavior.cs
@@ -0,0 +1,20 @@
+﻿using Google.XR.ARCoreExtensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OriginBehavior : MonoBehaviour
+{
+    public ARCloudAnchor cloudAnchor;
+
+    void Update()
+    {
+        // If cloudAnchor exists, then set the transform of this game object to the cloud anchors transform
+        if(cloudAnchor != null)
+        {
+            this.transform.position = Vector3.zero;
+            this.transform.rotation = Quaternion.identity;
+            this.transform.parent = cloudAnchor.transform;
+        }
+    }
+}
