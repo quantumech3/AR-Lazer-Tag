2c150da5b13c9c01814d1c399ecaaa6307e56c0a
quantumech3 <quantumech3@gmail.com>
2020-06-26 01:51:42 -0400

    Started implementing PlayerBehavior. Figuring out why players arent spawning

--- Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/LazerBehavior.cs
+++ Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/LazerBehavior.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 
 public class LazerBehavior : NetworkBehaviour
 {
+    // TODO: YOU MAY HAVE TO CHANGE HOW THIS COMPONENT WORKS IF THE LAZER POSITIONS ARE NOT PROPERLY SYNCED. YOU MIGHT NEED TO REPORT POSITION RELATIVE TO ORIGIN SIMILAR TO HOW PLAYERS DO IT
     public float speed = 0.1f;
 
     private void FixedUpdate()
--- Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/PlayerBehavior.cs
+++ Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/PlayerBehavior.cs
@@ -1,9 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.XR.ARFoundation;
 
-public class PlayerBehavior : MonoBehaviour
+public class PlayerBehavior : NetworkBehaviour
 {
+    [SyncVar]
+    public GameState state;
+
+    [SyncVar]
+    public double height;
+
+    [SyncVar]
+    public Vector3 position;
+
+    public GameObject pregameGuiPrefab;
+    public GameObject waitingGuiPrefab;
+    public GameObject aliveGuiPrefab;
+    public GameObject deadGuiPrefab;
+    public GameObject originPrefab;
+    public GameObject playerHitboxPrefab;
+    public GameObject lazerPrefab;
+    public GameObject arSessionOriginPrefab;
+    public GameObject arSessionPrefab;
+    public GameObject arExtensionsPrefab;
+    public ARNetworkManager networkManager;
+    public ServerInfo serverInfo;
+
+    private PlayerInfo playerInfo;
+    private GameObject origin;
+    private GameObject playerHitbox;
+    private GameObject camera;
+    private ARRaycastManager raycastManager;
+    private ARAnchorManager anchorManager;
+
+    [Command]
+    public void CmdSetHeight(double height)
+    {
+        this.height = height;
+    }
+
+    [Command]
+    public void CmdSetPlayerState(GameState state)
+    {
+        this.state = state;
+    }
+
+    [Command]
+    public void CmdSetServerState(GameState state)
+    {
+        this.serverInfo.state = state;
+    }
+
+    [Command]
+    public void CmdSetOriginId(string id)
+    {
+        this.serverInfo.originId = id;
+    }
+
+    [Command]
+    public void CmdSetPosition(Vector3 position)
+    {
+        this.position = position;
+    }
+
+    [Command]
+    public void CmdSpawnLazerAt(Vector3 position, Quaternion rotation)
+    {
+        GameObject lazer = Instantiate(lazerPrefab, position, rotation);
+        NetworkServer.Spawn(lazer);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +81,5 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
