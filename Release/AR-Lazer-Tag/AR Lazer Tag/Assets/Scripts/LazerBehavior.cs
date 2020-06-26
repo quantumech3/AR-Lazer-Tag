cd19d56404b6ac2de1e87ad69bec02196f14167f
quantumech3 <quantumech3@gmail.com>
2020-06-25 23:42:19 -0400

    Implemented the Lazer game object

---
+++ Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/LazerBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LazerBehavior : NetworkBehaviour
+{
+    public float speed = 0.1f;
+
+    private void FixedUpdate()
+    {
+        this.transform.position += this.transform.TransformDirection(new Vector3(0, 0, 1)).normalized * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }
+}
