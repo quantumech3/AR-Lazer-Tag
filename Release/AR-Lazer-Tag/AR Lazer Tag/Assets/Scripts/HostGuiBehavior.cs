4e89d00ff373095f4c8ff0d6a8d987aa06273b19
quantumech3 <quantumech3@gmail.com>
2020-06-25 22:01:37 -0400

    Implemented the Host Gui and completed the online scene

---
+++ Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/HostGuiBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class HostGuiBehavior : NetworkBehaviour
+{
+    public ARNetworkManager networkManager;
+    public ServerInfo serverInfo;
+    public Text gameStateDisplay;
+
+    void Update()
+    {
+        this.gameStateDisplay.text = serverInfo.state.ToString();
+    }
+}
---
+++ Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/ServerInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerInfo : NetworkBehaviour
+{
+    [SyncVar]
+    public GameState state;
+
+    [SyncVar]
+    public string originId;
+}
