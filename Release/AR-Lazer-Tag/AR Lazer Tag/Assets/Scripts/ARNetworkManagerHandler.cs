7485aeb1dca70a8971135f628c1cf130a8d302ca
quantumech3 <quantumech3@gmail.com>
2020-06-26 02:17:19 -0400

    Fixed the problem where players would not spawn by re-enabling "Dont Destroy On Load"

---
+++ Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/ARNetworkManagerHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARNetworkManagerHandler : MonoBehaviour
+{
+    public ARNetworkManager networkManager;
+
+    private void Start()
+    {
+        networkManager = GameObject.Find("AR Network Manager").GetComponent<ARNetworkManager>();
+    }
+
+    public void ClientStart()
+    {
+        networkManager.ClientStart();
+    }
+
+    public void ServerStart()
+    {
+        networkManager.ServerStart();
+    }
+
+    public void StopClient()
+    {
+        networkManager.StopClient();
+    }
+
+    public void StopServer()
+    {
+        networkManager.StopServer();
+    }
+}
