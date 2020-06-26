c533aca06aa37ef97696eb573b7ccc03f61c2c8b
quantumech3 <quantumech3@gmail.com>
2020-06-26 18:15:21 -0400

    Optimization: Made lazers despawn after being a configurable distance away from the origin

--- Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/LazerBehavior.cs
+++ Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/LazerBehavior.cs
@@ -8,12 +8,15 @@ public class LazerBehavior : NetworkBehaviour
 {
     public float speed = 0.1f;
     private bool transformIsLocalized = false;
+    private GameObject origin = null;
+    public float despawnRadius = 10;
 
     private void Start()
     {
         if (isClient && GameObject.Find("Origin(Clone)") != null)
         {
-            this.transform.SetParent(GameObject.Find("Origin(Clone)").transform, false);
+            this.origin = GameObject.Find("Origin(Clone)");
+            this.transform.SetParent(origin.transform, false);
             transformIsLocalized = true;
         }
     }
@@ -22,7 +25,8 @@ public class LazerBehavior : NetworkBehaviour
     {
         if (isClient && !transformIsLocalized && GameObject.Find("Origin(Clone)") != null)
         {
-            this.transform.SetParent(GameObject.Find("Origin(Clone)").transform, false);
+            this.origin = GameObject.Find("Origin(Clone)");
+            this.transform.SetParent(origin.transform, false);
             transformIsLocalized = true;
         }
     }
@@ -33,9 +37,15 @@ public class LazerBehavior : NetworkBehaviour
         {
             // Spawn a new lazer with its position shifted then delete this lazer.
             // I am doing this as a part of a work-around to localize position relative to origin
-            GameObject _lazer = Instantiate(this.gameObject);
-            _lazer.transform.position += _lazer.transform.forward * speed * Time.deltaTime;
-            NetworkServer.Spawn(_lazer);
+
+            // If this lazer is not over the despawn radius then move
+            if (this.transform.position.magnitude < despawnRadius)
+            {
+                GameObject _lazer = Instantiate(this.gameObject);
+                _lazer.transform.position += _lazer.transform.forward * speed * Time.deltaTime;
+                NetworkServer.Spawn(_lazer);
+            }
+                
             NetworkServer.Destroy(this.gameObject);
         }
     }
