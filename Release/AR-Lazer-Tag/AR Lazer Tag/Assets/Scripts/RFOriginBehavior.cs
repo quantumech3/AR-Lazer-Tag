73e45cfe735e8ef49d3d2c0e69ba5606746beba7
quantumech3 <quantumech3@gmail.com>
2021-01-30 10:59:31 -0500

    Add RF origin to level (RF origin rotation is calibrated according to user input)

---
+++ Release/AR-Lazer-Tag/AR Lazer Tag/Assets/Scripts/RFOriginBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RFOriginBehavior : MonoBehaviour
+{
+    public Slider xRot, yRot, zRot;
+    public string xRotSliderName, yRotSliderName, zRotSliderName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If all sliders exist, set rotation of this object to <xrot, yrot, zrot> in euler angles
+        if (xRot && yRot && zRot)
+        {
+            this.transform.rotation = Quaternion.Euler(new Vector3(xRot.value, yRot.value, zRot.value));
+        }
+        else
+        {
+            // Else attempt to find sliders in level (Only happens for clients in Alive State)
+            GameObject xSlider, ySlider, zSlider;
+            xSlider = GameObject.Find(xRotSliderName);
+            ySlider = GameObject.Find(yRotSliderName);
+            zSlider = GameObject.Find(zRotSliderName);
+
+            if (xSlider && ySlider && zSlider)
+            {
+                xRot = xSlider.GetComponent<Slider>();
+                yRot = ySlider.GetComponent<Slider>();
+                zRot = zSlider.GetComponent<Slider>();
+            }
+        }
+    }
+}
